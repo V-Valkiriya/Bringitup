fe2d828ae615e77d9a8d98235039a47941d43268
Valeriya <altufasu17@gmail.com>
2023-09-13 14:04:10 +0200

    links

--- src/js/modules/links.js
+++ src/js/modules/links.js
@@ -1,10 +1,13 @@
-// export default class Links {
-//     constructor() {
-//         this.modules = document.querySelectorAll('.module');
-//         this.links = document.querySelectorAll('.card');
-
-//     }
-//     init() {
-       
-//     }
-// }
+export default class Links {
+  constructor() {
+    this.modules = document.querySelectorAll(".module");
+    this.links = document.querySelectorAll(".card");
+  }
+  init() {
+    this.links.forEach((item) => {
+      for (let i = 0; i < this.modules.length; i++) {
+        item.location.hash = this.modules[i].id;
+      }
+    });
+  }
+}
